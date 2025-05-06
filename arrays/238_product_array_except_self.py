475232322a258b15c26973e901c3027432913616
Varun Jhaveri <varunjhaveri@1x-nat-vl931-172-30-136-138.wireless.umass.edu>
2025-05-06 17:59:57 -0400

    feat(arrays): add Product of Array Except Self solution (#238)

--- arrays/238_product_array_except_self.py
+++ arrays/238_product_array_except_self.py
@@ -1,5 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums):
-        lprod = [1]*len(nums)
-        rprod = [1]*len(nums)
-        pass
+        l,r = 1,1
+        lprod, rprod = [],[]
+        for i in range(len(nums)):
+            lprod.append(l)
+            l *= nums[i]
+            
+        for j in range(len(nums)-1,-1,-1):
+            rprod.append(r)
+            r *= nums[j]
+        
+        prod = []
+        for k in range(len(nums)):
+            prod.append(lprod[k] * rprod[::-1][k])
+        return prod
