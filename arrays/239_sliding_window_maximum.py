2c8b885c65f6c1df96918926e7b598f2a9260675
Varun Jhaveri <varunjhaveri@varuns-air.mynetworksettings.com>
2025-05-22 19:27:39 -0400

    feat(arrays): add Sliding Window Maximum Solution (#239)

---
+++ arrays/239_sliding_window_maximum.py
@@ -0,0 +1,14 @@
+import heapq
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        for i in range(len(nums)):
+            nums[i] = -nums[i]
+        i,j = 0,k
+        res = []
+        while j<=len(nums):
+            arr = nums[i:j]
+            heapq.heapify(arr)
+            res.append(-heapq.heappop(arr))
+            i+=1
+            j+=1
+        return res
