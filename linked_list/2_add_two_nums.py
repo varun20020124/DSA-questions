afd6c7fa128baa0ab1b74286f69b68e688b6263b
Varun Jhaveri <varunjhaveri@varuns-air.mynetworksettings.com>
2025-05-22 12:20:38 -0400

    feat(Linked List): add Add Two Numbers Solution (#2)

--- linked_list/2_add_two_nums.py
+++ linked_list/2_add_two_nums.py
@@ -4,4 +4,21 @@ class Node:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1, l2):
-        pass
+        carry = 0
+        dummy = Node()
+        curr = dummy
+        while l1 or l2:
+            total = carry
+            if l1: total += l1.val 
+            if l2: total += l2.val
+            node = Node(total%10)
+            carry = total//10
+            curr.next = node
+            curr = curr.next 
+            if l1 : l1 = l1.next
+            if l2 : l2 = l2.next
+        
+        if carry>0:
+            node = Node(carry)
+            curr.next = node
+        return dummy.next
