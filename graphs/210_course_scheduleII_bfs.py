665c728cbf888d03711bb9e2460bc69fa9526ecd
Varun Jhaveri <varunjhaveri@varuns-air.mynetworksettings.com>
2025-05-25 21:46:11 -0400

    feat(graphs): add Course Schedule II bfs (#210)

---
+++ graphs/210_course_scheduleII_bfs.py
@@ -0,0 +1,27 @@
+from collections import deque
+class Solution:
+    def findOrder(self, numCourses, prerequisites):
+        def topological(adj):
+            indegree = [0] * numCourses
+            for i in range(len(adj)):
+                for j in adj[i]:
+                    indegree[j] += 1
+            q = deque()
+            for i in range(len(indegree)):
+                if indegree[i] == 0:
+                    q.append(i) 
+            topo = []
+            while q:
+                node = q.popleft()
+                topo.append(node)
+                for neighbor in adj[node]:
+                    indegree[neighbor] -= 1
+                    if indegree[neighbor] == 0:
+                        q.append(neighbor)
+            return topo
+        adj = [[] for _ in range(numCourses)]
+        for edge in prerequisites:
+            adj[edge[1]].append(edge[0])
+        if len(topological(adj))!=numCourses:
+            return []
+        return topological(adj)
