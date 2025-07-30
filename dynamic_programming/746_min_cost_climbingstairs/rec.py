2684a80475f65e2842154cf8d02ec4d3109bb8f2
Varun Jhaveri <varunjhaveri@varuns-air.mynetworksettings.com>
2025-07-30 17:44:50 -0400

    Add DP/746 Mincost Climbing Stairs rec

--- dynamic_programming/746_min_cost_climbingstairs/rec.py
+++ dynamic_programming/746_min_cost_climbingstairs/rec.py
@@ -1,1 +1,8 @@
 class Solution:
+    def mincost(self,cost):
+        n = len(cost)
+        def rec(i):
+            if i>=n:
+                return 0
+            return cost[i] + min(rec(i+1),rec(i+2))
+        return min(rec(0),rec(1))
